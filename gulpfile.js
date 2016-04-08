e20cc82d381d5ee46017ae23c50f196588b24aa2
fabs <faebeee@gmail.com>
2016-04-08 22:48:14 +0200

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', function() {
+
+gulp.task('complex', function() {
   return gulp.src([
-    './lib/**/*.js',
+     './lib/**/*.js',
     './src/**/*.js',
     ])
     .pipe(concat('complex-stats.js'))
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', function() {
+    gulp.start('complex');
 });
